38ed5772270c57c46da8ac021edd32dbd52ab127
Roman Szlachtun <lidroma2@gmail.com>
2024-05-21 17:41:27 +0200

    final version zad3@lista4

--- lab4-concurrency/rust-impl/src/main.rs
+++ lab4-concurrency/rust-impl/src/main.rs
@@ -1,20 +1,18 @@
-use parking_lot::{Condvar, Mutex};
-use std::sync::{Arc};
+use parking_lot::Mutex;
+use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
 use rand::Rng;
 
 const PHILOSOPHER_COUNT: usize = 5;
 const DISH_COUNT: usize = 3;
-// const FORK_WAIT_TIMEOUT: Duration = Duration::from_millis(1000);
-const FORK_WAIT_TIMEOUT: Duration = Duration::from_millis(2);
+const FORK_WAIT_TIMEOUT: Duration = Duration::from_millis(1000);
 
 struct Philosopher {
     number: usize,
     left_fork: Arc<Mutex<()>>,
     right_fork: Arc<Mutex<()>>,
     dish_eaten: usize,
-    cvar: Arc<Condvar>,
 }
 
 impl Philosopher {
@@ -24,20 +22,20 @@ impl Philosopher {
             left_fork,
             right_fork,
             dish_eaten: 0,
-            cvar: Arc::from(Condvar::new()),
         }
     }
 
     fn eat(&mut self) {
         while self.dish_eaten < DISH_COUNT {
-            // self.cvar = Arc::from(Condvar::new());
             println!("Philosopher #{} is thinking", self.number);
-            // thread::sleep(Duration::from_millis(100 * (rand::thread_rng().gen_range(1..=10))));
-            thread::sleep(Duration::from_millis(1));
+            thread::sleep(Duration::from_millis(100 * (rand::thread_rng().gen_range(1..=10))));
 
             println!("Philosopher #{} is hungry", self.number);
             let left_fork = self.left_fork.lock();
 
+            // try to lock (take) fork in specified time
+            // if succeeded (Some), a philosopher can consume dish
+            // if failed (None), a philosopher puts their left fork back
             match self.right_fork.try_lock_for(FORK_WAIT_TIMEOUT) {
                 Some(_data) => {
                     println!("Philosopher #{} is eating", self.number);
@@ -48,8 +46,7 @@ impl Philosopher {
                 }
             }
 
-            // thread::sleep(Duration::from_millis(100 * (rand::thread_rng().gen_range(1..=10))));
-            thread::sleep(Duration::from_millis(1));
+            thread::sleep(Duration::from_millis(100 * (rand::thread_rng().gen_range(1..=10))));
             drop(left_fork);
         }
         println!("Philosopher #{} is leaving", self.number);
